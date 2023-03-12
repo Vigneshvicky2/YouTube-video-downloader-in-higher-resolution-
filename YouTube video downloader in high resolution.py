7543cb57002c6c742b7bb2d59626196f9c653356
Vigneshvicky2 <107059364+Vigneshvicky2@users.noreply.github.com>
2023-03-12 16:49:12 +0530

    Update YouTube video downloader in high resolution.py

--- YouTube video downloader in high resolution.py
+++ YouTube video downloader in high resolution.py
@@ -1,12 +1,14 @@
-
+#importing module
 from pytube import YouTube
+#getting link from user
 link=str(input("Enter your video link : "))
 yt=YouTube(link)
+#printing tile
 print("************YOUR VIDEO TITLE************")
 print(yt.title)
 print("please wait your video is now downloading....")
+#Downloading video
 resolution=yt.streams.filter(progressive='True',file_extension='mp4')
-
 resolution.get_highest_resolution().download()
 print("Download completed")
 print("This program was created by vignesh")
